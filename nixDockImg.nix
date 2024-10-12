df063dcb33a1e12123345b0d53fc24063fb1d351
Friederike Schertel <friederike@schertel.org>
2024-10-12 17:17:00 +0200

    howtobuild with nix

--- nixDockImg.nix
+++ nixDockImg.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; dockerTools.buildLayeredImage {
-  name = "hello";
+  name = "registry.gitlab.com/frie321984/frieweb";
   tag = "latest";
 
-  contents = [ cowsay bashInteractive coreutils catimg ];
+  contents = [ cowsay bashInteractive coreutils catimg python3 nodejs_22 lftp gnumake imagemagick ];
 
   config.Cmd = [ "/bin/bash" ];
 }
